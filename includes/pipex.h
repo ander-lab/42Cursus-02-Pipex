dac9e6440ff693b2f05eb084e70a8cee494c09a3
Alex <jimenezs.alex@gmail.com>
2021-10-18 17:08:33 +0200

    new fts

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: ajimenez <ajimenez@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/14 15:34:26 by ajimenez          #+#    #+#             */
-/*   Updated: 2021/10/18 15:44:35 by ajimenez         ###   ########.fr       */
+/*   Updated: 2021/10/18 17:08:23 by ajimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,9 @@
 #define READ_END	0
 #define WRITE_END	1 	
 
+/*
+** STRUCTURE
+*/
 typedef struct s_pipex{
 	char	**paths;
 	int		fd_1;
@@ -30,12 +33,15 @@ typedef struct s_pipex{
 } t_pipex;
 
 /*
-**
+** CMD FUNCTIONS
 */
 char	*path_cmd(t_pipex *ps, char **envp, char *cmd);
 void	ft_exec_cmd1(t_pipex *ps, char **env, int *fd);
-void	get_args(int ac, char **av, t_pipex *ps, char **env);
 void	ft_exec_cmd1(t_pipex *ps, char **env, int *fd);
 void	ft_exec_cmd2(t_pipex *ps, char **env, int *fd);
+/*
+** GET ARGS
+*/
+void	get_args(int ac, char **av, t_pipex *ps, char **env);
 
 #endif
