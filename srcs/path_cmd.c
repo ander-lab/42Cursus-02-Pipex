bae3d45f22333c6605cb95a2f0d9af9a1536a748
Alex <jimenezs.alex@gmail.com>
2021-10-21 19:23:21 +0200

    dup

--- srcs/path_cmd.c
+++ srcs/path_cmd.c
@@ -6,7 +6,7 @@
 /*   By: ajimenez <ajimenez@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/17 16:13:34 by ajimenez          #+#    #+#             */
-/*   Updated: 2021/10/21 17:23:27 by ajimenez         ###   ########.fr       */
+/*   Updated: 2021/10/21 19:22:53 by ajimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,6 +59,14 @@ void	exec_cmd(t_pipex *ps, char *env[])
 	execve(path, cmd, env);
 }
 
+/* ************************************************************************** */
+/*  STDOUT_FILENO = 1                                                         */
+/*  STDIN_FILENO  = 0                                                         */
+/*  WRITE_END     = 1                                                         */
+/*  READ_END      = 0                                                         */
+/* ************************************************************************** */
+
+
 void	ft_exec_cmd1(t_pipex *ps, char **env, int *fd)
 {
 	char	*path;
@@ -71,13 +79,12 @@ void	ft_exec_cmd1(t_pipex *ps, char **env, int *fd)
 		ft_open_error("error ", ps);
 	path = path_cmd(ps, env, ps->cmd_1);/*recoger comandos*/
 	cmd = ft_split(ps->cmd_1, ' ');/*split del comando por si hay argumentos*/
-
 	
 
-	dup2(fd[1], 1);/**/
-	//close(fd[WRITE_END]);
-	dup2(ps->fd_2, 1);
-	//close(ps->fd_1);
+	dup2(ps->fd_1, 0);
+	close(ps->fd_1);
+	dup2(fd[1], STDOUT_FILENO);/**/
+	close(fd[1]);
 	execve(path, cmd, env);
 }
 
