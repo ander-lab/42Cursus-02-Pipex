0c1e9600c56d58b6681142cf9bc7fbd998d318d0
Alex <jimenezs.alex@gmail.com>
2021-10-22 09:54:22 +0200

    include exec child

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: ajimenez <ajimenez@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/14 15:34:26 by ajimenez          #+#    #+#             */
-/*   Updated: 2021/10/21 17:08:40 by ajimenez         ###   ########.fr       */
+/*   Updated: 2021/10/22 09:54:07 by ajimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,8 +37,8 @@ typedef struct s_pipex{
 */
 char	*path_cmd(t_pipex *ps, char **envp, char *cmd);
 void	ft_exec_cmd1(t_pipex *ps, char **env, int *fd);
-void	ft_exec_cmd1(t_pipex *ps, char **env, int *fd);
 void	ft_exec_cmd2(t_pipex *ps, char **env, int *fd);
+void	ft_exec_2child(t_pipex *ps, char **env, int *fd, int pid);
 /*
 ** GET ARGS
 */
